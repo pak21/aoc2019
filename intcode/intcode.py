01bc538f12e0438c936a0482ec611b3fdafda815
Phil Kendall <phil.kendall@controlf1.co.uk>
2019-12-02 15:35:38 +0000

    Add memory to intcode.Program object.

--- intcode/intcode.py
+++ intcode/intcode.py
@@ -1,7 +1,12 @@
 class Program():
-    def __init__(self):
+    def __init__(self, initial_memory):
         self._pc = 0
+        self._memory = initial_memory.copy()
 
     @property
     def pc(self):
         return self._pc
+
+    @property
+    def memory(self):
+        return self._memory
--- intcode/tests/test_intcode.py
+++ intcode/tests/test_intcode.py
@@ -5,10 +5,23 @@ import intcode
 class Test_Program(unittest.TestCase):
     def test_pc_is_initially_zero(self):
         # Arrange
-        program = intcode.Program()
+        program = intcode.Program([])
 
         # Act
         pc = program.pc
 
         # Assert
         self.assertEqual(pc, 0)
+
+    def test_memory_is_copied(self):
+        # Arrange
+        initial_value = 42
+        initial_memory = [initial_value]
+        memory_offset = 0
+        program = intcode.Program(initial_memory)
+
+        # Act
+        program.memory[memory_offset] = 0
+
+        # Assert
+        self.assertEqual(initial_memory[memory_offset], initial_value)
