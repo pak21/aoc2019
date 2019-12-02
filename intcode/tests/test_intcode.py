a41dfb827f5d8e540cc4566f9677f335e8bd32c3
Philip Kendall <philip@shadowmagic.org.uk>
2019-12-02 16:58:16 +0000

    Add Day 2 integration tests.

--- intcode/tests/test_intcode.py
+++ intcode/tests/test_intcode.py
@@ -83,3 +83,23 @@ class Test_Program(unittest.TestCase):
 
         # Assert
         self.assertEqual(program.memory, expected)
+
+class Test_Program_Day2(unittest.TestCase):
+    """Integration tests from Advent of Code Day 2"""
+
+    @parameterized.expand([
+        ([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50], [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]),
+        ([1, 0, 0, 0, 99], [2, 0, 0, 0, 99]),
+        ([2, 3, 0, 3, 99], [2, 3, 0, 6, 99]),
+        ([2, 4, 4, 5, 99, 0], [2, 4, 4, 5, 99, 9801]),
+        ([1, 1, 1, 4, 99, 5, 6, 0, 99], [30, 1, 1, 4, 2, 5, 6, 0, 99]),
+    ])
+    def test_example(self, initial_memory, expected):
+        # Arrange
+        program = intcode.Program(initial_memory)
+
+        # Act
+        program.run()
+
+        # Assert
+        self.assertEqual(program.memory, expected)
