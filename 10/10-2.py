6bd9f75f77bf583926745418026f848cf562ad7f
Phil Kendall <phil.kendall@controlf1.co.uk>
2019-12-10 16:44:17 +0000

    Tidy up.

--- 10/10-2.py
+++ 10/10-2.py
@@ -6,21 +6,13 @@ import sys
 import numpy as np
 
 def ordering(p, laser_location):
-    if p == laser_location:
-        return (1000, 0) # Invalid angle so can be ignored later
-
     dy = laser_location[0] - p[0]
     dx = p[1] - laser_location[1]
 
-    angle = math.pi / 2 - math.atan2(dy, dx)
-    if angle < 0:
-        angle += 2 * math.pi
-    elif angle > 2 * math.pi:
-        angle -= 2 * math.pi
-
+    angle = (math.pi / 2 - math.atan2(dy, dx)) % (2 * math.pi)
     distance = abs(dy) + abs(dx)
 
-    return (angle, distance)
+    return (angle, distance, p)
 
 def valid(data, p):
     return p[0] >= 0 and p[0] < data.shape[0] and p[1] >= 0 and p[1] < data.shape[1]
@@ -30,33 +22,24 @@ with open(sys.argv[1]) as f:
 
 laser = (int(sys.argv[2]), int(sys.argv[3]))
 
-foo = []
-for y in range(data.shape[0]):
-    for x in range(data.shape[1]):
-        o = ordering((y, x), laser)
-        foo.append((*o, (y, x)))
-
-foo = sorted([f for f in foo if f[0] != 1000])
-
-total = np.sum(data) - 1
+ordered_positions = sorted([ordering(location, laser) for location in np.ndindex(data.shape) if location != laser])
 
 last_vaporized_angle = None
 next_index = 0
 vaporized = 0
 
 while True:
-    angle, distance, location = foo[next_index]
+    angle, distance, location = ordered_positions[next_index]
     if data[location]:
         vaporized += 1
+        if vaporized == 200:
+            print('Vaporizing asteroid {} at {} -> {}'.format(vaporized, location, 100 * location[1] + location[0]))
+            break
         last_vaporized_angle = angle
-        print('Vaporizing asteroid {} at {}, angle {}'.format(vaporized, location, angle))
         data[location] = False
 
-        if vaporized == total:
-            break
-
         # Move to next angle
-        while foo[next_index][0] == last_vaporized_angle:
-            next_index = (next_index + 1) % len(foo)
+        while ordered_positions[next_index][0] == last_vaporized_angle:
+            next_index = (next_index + 1) % len(ordered_positions)
     else:
-        next_index = (next_index + 1) % len(foo)
+        next_index = (next_index + 1) % len(ordered_positions)
--- 10/10.py
+++ 10/10.py
@@ -7,49 +7,43 @@ import numpy as np
 def valid(data, p):
     return p[0] >= 0 and p[0] < data.shape[0] and p[1] >= 0 and p[1] < data.shape[1]
 
-def fourdiffs(location, distance, d):
+def fourdiffs(distance, d):
     yield (-distance  , -distance+d)
     yield (-distance+d,  distance  )
     yield ( distance  ,  distance-d)
     yield ( distance-d, -distance  )
 
 def analyze(data, location):
-    if not data[location]:
-        return 0
-
     count = 0
     seen = set()
 
     for distance in range(1, max(data.shape)):
         for d in range(2*distance):
-            for diff in fourdiffs(location, distance, d):
+            for diff in fourdiffs(distance, d):
                 nextpos = location
                 blocked = False
                 while True:
                     nextpos = (nextpos[0] + diff[0], nextpos[1] + diff[1])
-                    if not valid(data, nextpos):
-                        break
-                    if nextpos in seen:
+                    if not valid(data, nextpos) or nextpos in seen:
                         break
                     seen.add(nextpos)
-                    if data[nextpos]:
-                        if not blocked:
-#                            print('Seen at {}'.format(nextpos))
-                            blocked = True
-                            count += 1
-                        else:
-#                            print('Blocked at {}'.format(nextpos))
-                            pass
+                    if data[nextpos] and not blocked:
+                        blocked = True
+                        count += 1
 
     return count
 
 with open(sys.argv[1]) as f:
     data = np.array([[c == '#' for c in l.strip()] for l in f])
 
-maxseen = 0
-for y in range(data.shape[0]):
-    for x in range(data.shape[1]):
-        seen = analyze(data, (y, x))
+maxseen = -1
+maxlocation = None
+
+for location, value in np.ndenumerate(data):
+    if value:
+        seen = analyze(data, location)
         if seen > maxseen:
-            print('Saw {} asteroids at {}'.format(seen, (y, x)))
             maxseen = seen
+            maxlocation = location
+
+print('Saw {} asteroids at {}'.format(maxseen, maxlocation))
